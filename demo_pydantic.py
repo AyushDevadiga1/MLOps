c031bfe337291c2f94bf728250f326fa6243b2d9
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-02-07 20:13:45 +0530

    Using the functions : Different formats - EmailStr,AnyUrl; Fields for validation and new conditons; Specifying the DataTypes while instantiating the object attributes; Use of Annotated and how it is used to provide metadata for the object attributes

--- demo_pydantic.py
+++ demo_pydantic.py
@@ -29,21 +29,58 @@ load_pokemon('ghastly','one')
 
 # Now rewrittng with pydantic version
 
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Optional,Annotated
 
 # Always use BaseModel as we dont have __init_ function and use hints to validate dtype instead .
 class Pokemon(BaseModel):
 
-    # Defined schema / Rules
-    name : str 
-    age : int 
+    # Defined schema / Rules for Data Validation
+    # By default the values are necessary when object is instantiated and if any of the value is not passed we would get a error.
+    # The Field keyword is use to set data constraints / Custom Data Validation
+
+    # We use Annotated to provide some metadata ; syntax : Annotated[dtype,Field(Params)]
+
+    name : Annotated[
+                        str,Field(
+                        max_length=20,
+                        title='Name of the pokemon',
+                        description='The Name must be of type `str` and must be of max length 20',
+                        examples=['haunter','gangar']
+                        )
+                    ]
+    age : int = Field(gt=0 ,lt=25)
+    attack : int = Field(gt=0 ,lt=100)
+    defense : int = Field(gt=0 ,lt=100)
+    desc : List[str] # We are validating the container and the elemnt also (2Way).
+    legendary : Annotated[
+                            bool,Field(
+                            default=False,
+                            strict=True  # No Type Conversion
+                            )
+                    ]
+    photo : AnyUrl
+    # email : EmailStr
 
 # Our Main function
 def load_pokemon(pokemon : Pokemon): # Now instead of getting the variables independently we get a Pokemon object , so we modify the logic accordingly.
-    print(f'{pokemon.name}\n{pokemon.age}')
+    print(f'{pokemon.name}')
+    print(pokemon.legendary) 
     print('Variables validated successfuly !')
 
-pokemon_info = { 'name' : 'Ghastly' , 'age' : 'hellow'}
+ # It is smart and will convert '20' if passed into int field automatically.
+pokemon_info = {    
+                    'name' : 'Ghastly' ,
+                    'age' : 1 ,
+                    'attack' : 68,
+                    'defense' : 70,
+                    'desc' : [
+                                    'A cool Ghost type pokemon .'
+                    ],
+                    'legendary' : True,
+                    'photo' : 'https://www.nicepng.com/maxp/u2w7w7q8q8w7w7e6/'
+                       
+               }
 
 poke1 = Pokemon(**pokemon_info)
 load_pokemon(poke1)
