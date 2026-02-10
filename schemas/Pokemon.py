c2c306af40ac9db61142f48ac8e3c11b61f11d1f
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-02-10 19:51:36 +0530

    Modularity Changes 1

---
+++ schemas/Pokemon.py
@@ -0,0 +1,63 @@
+from typing import List,Annotated
+from pydantic import BaseModel,Field
+
+# The class to which we are gonna send variables when validating
+class Pokemon(BaseModel):
+    # Now the variables here will be the same as the variables in pokemon.json
+    id : Annotated[
+                    str,Field(  
+                                ...,
+                                description='An Integer value ID for the pokemon',
+                                examples = ['10']
+                    )
+    ]
+    name : Annotated[
+                    str,Field(  
+                                ...,
+                                description='The name of the Pokemon',
+                                examples = ['Raichu']
+                    )
+    ]
+    types : Annotated[
+                    List[str],Field(  
+                                ...,
+                                description='A list of values which represent the type of the pokemon',
+                                examples = [['Grass' , 'Water']]
+                    )
+    ]
+    hp : Annotated[
+                    int,Field(  
+                                ...,
+                                description='An integer representing the  HP value of the pokemon',
+                                examples = ['69'],
+                                le=255,
+                                ge=0
+                    )
+    ]
+    attack : Annotated[
+                    int,Field(  
+                                ...,
+                                description='An integer representing the attack stat of the pokemon',
+                                examples = ['33'],
+                                le=255,
+                                ge=0
+                    )
+    ]
+    defense : Annotated[
+                    int,Field(  
+                                ...,
+                                description='An integer representing the defense stat of the pokemon',
+                                examples = ['22'],
+                                le=255,
+                                ge=0
+                    )
+    ]
+    speed : Annotated[
+                    int,Field(  
+                                ...,
+                                description='An integer representing the speed  of the pokemon',
+                                examples = ['98'],
+                                le=150,
+                                ge=0
+                    )
+    ]
